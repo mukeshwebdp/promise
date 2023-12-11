46b2cabde632cea5c8823b731057fd36778f6aea
mukeshwebdp <mukesh.webdp@gmail.com>
2023-12-11 19:45:13 +0530

    task four complete

--- promises.js
+++ promises.js
@@ -10,6 +10,7 @@ const promiseOne = new Promise(function(resolve,reject){
 promiseOne.then(function(){
     console.log('Promise consumed');
 })
+        //API
 
 // const requestUrl = 'https://api.github.com/users/mukeshwebdp'
 // const xhr = new XMLHttpRequest()
@@ -30,4 +31,38 @@ new Promise(function(resolve,reject){
     },1000)
 }).then(function(){
     console.log('Promise consumed two');
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('Asyne task 3');
+        resolve({name: 'Mukesh',email: 'mukeshwebdp@gamil.com'})
+    },1000)
+})
+
+promiseThree.then(function(users){
+    console.log(users);
+})
+
+const promisefour = new Promise(function(resolve,reject){
+   setTimeout(function(){
+    const error = false;
+    console.log('Async task 4');
+    if(!error){
+        resolve({username: 'Mukesh',email : 'mukesh@gmial.com'})
+    }else{
+        reject('Error: Something went wrong')
+    }
+   },1000)
+})
+
+promisefour.then(function(user){
+    console.log(user);
+    return user.username
+}).then(function(username){
+    console.log(username);
+}).catch(function(){
+    console.log('error');
+}).finally(function(){
+    console.log('The promise is either resolved or rejected');
 })
